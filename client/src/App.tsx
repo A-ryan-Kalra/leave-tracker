1987c2ae4e2ce62e17bfa58bee3ca73503801334
Aryan Kalra <aryan.smart832@gmail.com>
2025-08-30 20:28:09 +0530

    setup routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import GoogleLogin from "./components/google-login";
+import Hero from "./components/hero";
+import PageNotFound from "./components/page-not-found";
+import Dashboard from "./components/dashboard";
+import ErrorBoundary from "./components/error-boundary";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Hero />,
+    errorElement: <ErrorBoundary />,
   },
   {
-    path: "/hello",
-    element: <div>Hello </div>,
+    path: "/login",
+    element: <GoogleLogin />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+    errorElement: <ErrorBoundary />,
   },
 ]);
 function App() {
