55bd43fff7933bb4636a7646ddb09ed6cdce357f
Aryan Kalra <aryan.smart832@gmail.com>
2025-08-31 17:50:47 +0530

    add events in calender

--- client/src/components/dashboard-feature/alert_dialog/show-leave-dialog.tsx
+++ client/src/components/dashboard-feature/alert_dialog/show-leave-dialog.tsx
@@ -14,6 +14,9 @@ import SelectLeaveType from "../select-leave-type";
 import { Textarea } from "@/components/ui/textarea";
 import type { CalendarEvent, startEndDateType } from "type";
 import moment from "moment";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface ShowAlertDialTypes {
   open: boolean;
@@ -22,48 +25,95 @@ interface ShowAlertDialTypes {
   startEndDate: startEndDateType;
 }
 
-function ShowLeaveDialog({ open, setOpen, startEndDate }: ShowAlertDialTypes) {
+function ShowLeaveDialog({
+  open,
+  setOpen,
+  startEndDate,
+  events,
+}: ShowAlertDialTypes) {
+  const [details, setDetails] = useState({ reason: "", leaveType: "" });
+  console.log(details);
+
+  function handleSubmit() {
+    const payload = {
+      ...details,
+      id: Date.now().toString(),
+      start: startEndDate.start,
+      end: startEndDate.end,
+      totalDay: startEndDate.totalDay?.toString(),
+    };
+    events(payload);
+    setOpen();
+  }
+
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
       {/* <AlertDialogTrigger>Open</AlertDialogTrigger> */}
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Event Details</AlertDialogTitle>
-          <AlertDialogDescription className="my-5 flex flex-col gap-y-7">
-            <div className="flex gap-y-2 items-start flex-col">
+          <AlertDialogDescription className="my-5 flex w-full flex-col gap-y-7">
+            <div className="flex gap-y-2 w-full justify-around  items-center text-lg ">
+              <div className="flex flex-col">
+                <Label>Start</Label>
+                <input
+                  disabled
+                  type="text"
+                  value={moment(startEndDate?.start).format("MMMM D, YYYY")}
+                />
+              </div>
+              <div className="flex flex-col">
+                <Label>End</Label>
+                <input
+                  disabled
+                  type="text"
+                  value={moment(
+                    new Date(startEndDate?.end).setDate(
+                      startEndDate?.end.getDate() - 1
+                    )
+                  ).format("MMMM D, YYYY")}
+                />
+              </div>
+            </div>
+            <div className="flex w-full gap-y-2 items-start flex-col">
               <Label htmlFor="reason">Reason</Label>
-              <Textarea id="reason" placeholder="Enter Reason" />
+              <Textarea
+                className="max-sm:w-[300px]"
+                id="reason"
+                onChange={(e) =>
+                  setDetails((prev) => ({ ...prev, reason: e.target.value }))
+                }
+                placeholder="Enter Reason"
+              />
             </div>
 
-            <div className="flex gap-y-3  w-full items-center justify-between">
-              <div className="flex gap-y-2 w-fit  items-start flex-col">
+            <div className="flex gap-y-3 max-sm:gap-x-2 items-center justify-between max-sm:justify-start">
+              <div className="flex gap-y-2     items-start flex-col">
                 <Label>Leave Type</Label>
-                <SelectLeaveType />
+                <SelectLeaveType
+                  type={(value: string) =>
+                    setDetails((prev) => ({ ...prev, leaveType: value }))
+                  }
+                />
               </div>
-
-              <div className="flex gap-y-2 w-full   items-start ">
-                <div className="flex flex-col">
-                  <Label>Start</Label>
-                  <input
-                    type="text"
-                    value={moment(startEndDate?.start).format("MMMM D, YYYY")}
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <Label>End</Label>
-                  <input
-                    type="text"
-                    value={moment(startEndDate?.end).format("MMMM D, YYYY")}
-                  />
-                </div>
+              <div className="flex gap-y-2    items-start flex-col">
+                <Label>Total Days</Label>
+                <Input
+                  className="max-sm:w-[150px]"
+                  value={startEndDate.totalDay?.toString()}
+                />
               </div>
             </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
+        <div className="flex gap-y-3 gap-x-2 items-center justify-end max-sm:justify-start">
+          <AlertDialogCancel className="cursor-pointer">
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleSubmit} className="cursor-pointer">
+            Submit
+          </AlertDialogAction>
+        </div>
       </AlertDialogContent>
     </AlertDialog>
   );
