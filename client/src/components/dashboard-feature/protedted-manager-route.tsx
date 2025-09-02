62e5fe513fb8d26ee451420024a42abd098b0e84
Aryan Kalra <aryan.smart832@gmail.com>
2025-09-03 01:57:03 +0530

    add manager routes

--- client/src/components/dashboard-feature/protedted-manager-route.tsx
+++ client/src/components/dashboard-feature/protedted-manager-route.tsx
@@ -1,13 +1,13 @@
 import { useUserData } from "@/hooks/user-data";
 import { Navigate } from "react-router";
-export default function ProtectedAdminRoute({
+export default function ProtectedManagerRoute({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const storeData = useUserData();
   const userData = storeData?.data;
-  if (userData?.role !== "ADMIN") {
+  if (userData?.role !== "MANAGER" && userData?.role !== "ADMIN") {
     return <Navigate to="/dashboard/me" replace />; // or return a forbidden message, etc.
   }
   return children;
---
+++ client/src/components/manager/pending.tsx
@@ -0,0 +1,151 @@
+import { useUserData } from "@/hooks/user-data";
+import { api } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+
+import { type ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, Loader, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
+import { DataTable } from "../ui/data-table";
+export type LeaveRequest = {
+  id: string;
+  startDate: string;
+  endDate: string;
+  reason: string;
+  leaveType: string;
+  manager: string;
+};
+
+function ManagePendingRequest() {
+  const storeData = useUserData();
+  const userData = storeData?.data;
+
+  // Mapper: JSON → LeaveRequest[]
+  function mapLeaveRequests(raw: any[]): LeaveRequest[] {
+    return raw?.map((leave) => ({
+      id: leave?.id,
+      startDate: leave?.startDate?.split("T")[0],
+      endDate: leave?.endDate?.split("T")[0],
+      reason: leave?.reason,
+      leaveType: leave?.leaveType?.name ?? "-",
+      manager: leave?.user?.groups?.[0]?.group?.manager?.fullName ?? "-",
+    }));
+  }
+
+  const { data, error, isLoading, isError, refetch } = useQuery({
+    queryKey: ["leaveRequests-pending"],
+    queryFn: listLeaveRequest,
+  });
+
+  async function listLeaveRequest() {
+    const response = await api.get(
+      `/dashboard/list-leave-request/${userData?.id}?status=PENDING`
+    );
+    return response.data;
+  }
+
+  const leaveColumns: ColumnDef<LeaveRequest>[] = [
+    {
+      accessorKey: "startDate",
+      header: ({ column }) => (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Start Date
+          <ArrowUpDown />
+        </Button>
+      ),
+    },
+    {
+      accessorKey: "endDate",
+      header: "End Date",
+    },
+    {
+      accessorKey: "reason",
+      header: "Reason",
+    },
+    {
+      accessorKey: "leaveType",
+      header: "Leave Type",
+    },
+    {
+      accessorKey: "manager",
+      header: "Manager",
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      // header: "Action",
+
+      cell: ({ row }) => {
+        const request = row.original;
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+              {/* <DropdownMenuItem>View Profile</DropdownMenuItem> */}
+              <DropdownMenuItem
+                onClick={async () => {
+                  try {
+                    await api.patch(
+                      `/dashboard/delete-leave-request/${userData?.id}?leaveRequestId=${request?.id}`
+                    );
+                    toast("Success", {
+                      description: `Leave request cancelled`,
+                      style: { backgroundColor: "white", color: "black" },
+                      richColors: true,
+                    });
+                    refetch();
+                  } catch (error) {
+                    console.error(error);
+                  }
+                }}
+              >
+                Cancel Request
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
+  if (isLoading) {
+    return (
+      <div className="flex w-full justify-center items-center">
+        <div className="animate-spin mt-10">
+          <Loader />
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data);
+  const tableData = mapLeaveRequests(data?.leaveRequests);
+  return (
+    <DataTable columns={leaveColumns} data={tableData} filterColumn="reason" />
+  );
+}
+
+export default ManagePendingRequest;
