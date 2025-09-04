1f9f76fd7f85ed3b717dcea99beec968e0bf10c3
Aryan Kalra <aryan.smart832@gmail.com>
2025-09-04 17:26:45 +0530

    changes

--- client/src/components/landing-page/footer.tsx
+++ client/src/components/landing-page/footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
           Made with ❤️ &copy; {new Date().getFullYear()} Leave Tracker
           <Link
             target="__blank"
-            to={"https://github.com/A-ryan-Kalra/huddle-hub"}
+            to={"https://github.com/A-ryan-Kalra/leave-tracker"}
             className="bg-slate-300 rounded-full p-1 hover:bg-slate-200 cursor-pointer transition"
           >
             <Github className="fill-white w-4 sm:w-5 h-4 sm:h-5" />
