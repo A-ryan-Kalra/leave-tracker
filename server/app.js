d7382b688ec8251d68d429bd9e527218bcf1361b
Aryan Kalra <aryan.smart832@gmail.com>
2025-09-10 16:10:20 +0530

    fixes

--- server/app.js
+++ server/app.js
@@ -15,12 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 app.get("/healthz", (req, res) => {
   res.send({ message: "Healthy.." });
