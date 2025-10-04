59e5cd8d26911778b4a679ec80de01181a8cec3f
Aryan Kalra <aryan.smart832@gmail.com>
2025-10-04 18:17:04 +0530

    add response interceptor

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,16 +2,36 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
+  withCredentials: true,
 });
 // In your api.ts, add interceptor:
 api.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("user-info");
+
   if (userInfo) {
     const { token } = JSON.parse(userInfo);
     config.headers.authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  async function err(err) {
+    const orig = err.config;
+    if (err.response.status === 401 && !orig._retry) {
+      orig._retry = true;
+      const { data } = await api.post(
+        "/refresh",
+        {},
+        { withCredentials: true }
+      );
+      localStorage.setItem("user-info", data.accessToken);
+      return api(orig);
+    }
+    return Promise.reject(err);
+  }
+);
 
 export const googleAuth = (code: string) =>
   api.get(`/auth/google?code=${code}`);
